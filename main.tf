d3c2a38503ba234d31dd663b3901658ae0f17f16
Christian Darvin <110832080+christiandarvs@users.noreply.github.com>
2025-07-25 11:18:30 +0800

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "main" {
-  ami                    = var.ami_id
-  instance_type          = var.ec2_instance_type
-  key_name               = "Sample-Web-Key"
-  user_data              = file("init_script.sh")
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
+  ami                         = var.ami_id
+  instance_type               = var.ec2_instance_type
+  key_name                    = "Sample-Web-Key"
+  user_data                   = file("init_script.sh")
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
   tags = {
     "Name" = "Sample-Website"
   }
@@ -12,11 +12,18 @@ resource "aws_instance" "main" {
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+  description = "Allow SSH and HTTP inbound traffic"
 
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
